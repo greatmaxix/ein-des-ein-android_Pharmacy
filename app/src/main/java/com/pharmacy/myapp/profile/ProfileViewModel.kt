ddbca4d9e42915c6af0e6a57ac6467fbbd1e1015
Max Lituchiy <lituchiy.maxim@gmail.com>
2020-07-27 17:28:27 +0300

    fix merge

--- app/src/main/java/com/pharmacy/myapp/profile/ProfileViewModel.kt
+++ app/src/main/java/com/pharmacy/myapp/profile/ProfileViewModel.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavDirections
 import com.bumptech.glide.Glide
 import com.pharmacy.myapp.core.base.mvvm.BaseViewModel
+import com.pharmacy.myapp.core.extensions.getMultipartBody
 import com.pharmacy.myapp.core.general.SingleLiveEvent
 import com.pharmacy.myapp.core.network.ResponseWrapper.Error
 import com.pharmacy.myapp.core.network.ResponseWrapper.Success
@@ -24,11 +25,7 @@ import kotlinx.coroutines.withContext
 import java.io.File
 
 
-class ProfileViewModel(
-    private val context: Context,
-    private val context: Context, private val repository: ProfileRepository
-) :
-    BaseViewModel() {
+class ProfileViewModel(private val context: Context, private val repository: ProfileRepository) :BaseViewModel() {
 
     val errorLiveData by lazy { SingleLiveEvent<String>() }
     val progressLiveData by lazy { SingleLiveEvent<Boolean>() }
