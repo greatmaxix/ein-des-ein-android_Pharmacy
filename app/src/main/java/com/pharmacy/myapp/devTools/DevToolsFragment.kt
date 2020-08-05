ed1687809c4d354b01b4a6e9598607b1b16a4cb8
Max Lituchiy <lituchiy.maxim@gmail.com>
2020-08-05 16:50:19 +0300

    added guest profile screen

--- app/src/main/java/com/pharmacy/myapp/devTools/DevToolsFragment.kt
+++ app/src/main/java/com/pharmacy/myapp/devTools/DevToolsFragment.kt
@@ -7,6 +7,7 @@ import com.pharmacy.myapp.core.base.mvvm.BaseMVVMFragment
 import com.pharmacy.myapp.core.extensions.onClick
 import com.pharmacy.myapp.devTools.DevToolsFragmentDirections.Companion.globalToCart
 import com.pharmacy.myapp.devTools.DevToolsFragmentDirections.Companion.globalToCheckout
+import com.pharmacy.myapp.devTools.DevToolsFragmentDirections.Companion.globalToGuestProfile
 import com.pharmacy.myapp.devTools.DevToolsFragmentDirections.Companion.globalToOrder
 import com.pharmacy.myapp.devTools.DevToolsFragmentDirections.Companion.globalToProductCard
 import kotlinx.android.synthetic.main.fragment_dev_tools.*
@@ -30,6 +31,10 @@ class DevToolsFragment(private val viewModel: DevToolsViewModel) : BaseMVVMFragm
 
         toOrder.onClick {
             navController.navigate(globalToOrder())
+        }
+
+        toGuestProfile.onClick {
+            navController.navigate(globalToGuestProfile())
         }
     }
 }
