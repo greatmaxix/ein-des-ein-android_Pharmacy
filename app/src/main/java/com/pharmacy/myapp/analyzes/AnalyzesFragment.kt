464bb3e70f8c228c96986437c4751388eff78fc2
Max Lituchiy <lituchiy.maxim@gmail.com>
2020-08-19 11:40:38 +0300

    fix PR comment

--- app/src/main/java/com/pharmacy/myapp/analyzes/AnalyzesFragment.kt
+++ app/src/main/java/com/pharmacy/myapp/analyzes/AnalyzesFragment.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import android.view.View
 import com.pharmacy.myapp.R
 import com.pharmacy.myapp.core.base.mvvm.BaseMVVMFragment
+import com.pharmacy.myapp.core.extensions.toast
 import kotlinx.android.synthetic.main.fragment_analyzes.*
 
 class AnalyzesFragment: BaseMVVMFragment(R.layout.fragment_analyzes) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        showBackButton(R.drawable.ic_arrow_back)
+        showBackButton()
         emptyContentAnalyzes.setButtonAction {
-
+            requireContext().toast("TODO Записаться на анализы")
         }
     }
 
