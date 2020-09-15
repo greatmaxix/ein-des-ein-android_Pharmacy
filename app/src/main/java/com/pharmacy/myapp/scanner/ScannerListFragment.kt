5b59ff0535a405e6b6e5a3e719e6562e098089a0
Max Lituchiy <lituchiy.maxim@gmail.com>
2020-09-15 17:00:50 +0300

    small fixes

--- app/src/main/java/com/pharmacy/myapp/scanner/ScannerListFragment.kt
+++ app/src/main/java/com/pharmacy/myapp/scanner/ScannerListFragment.kt
@@ -6,7 +6,7 @@ import androidx.navigation.fragment.navArgs
 import com.pharmacy.myapp.R
 import com.pharmacy.myapp.product.BaseProductFragment
 import com.pharmacy.myapp.product.ProductViewModel
-import kotlinx.android.synthetic.main.fragment_products_result.*
+import kotlinx.android.synthetic.main.fragment_scanner_result.*
 
 class ScannerListFragment(private val viewModel: ProductViewModel) : BaseProductFragment<ProductViewModel>(R.layout.fragment_scanner_result, viewModel) {
 
