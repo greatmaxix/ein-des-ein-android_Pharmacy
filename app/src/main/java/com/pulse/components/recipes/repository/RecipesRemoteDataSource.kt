1600131aadb69c34537004da3a181c72e61e9e0d
Ivan <ivan.kovalenko13@gmail.com>
2020-11-17 17:17:09 +0200

    - PR fix

--- app/src/main/java/com/pulse/components/recipes/repository/RecipesRemoteDataSource.kt
+++ app/src/main/java/com/pulse/components/recipes/repository/RecipesRemoteDataSource.kt
@@ -4,5 +4,5 @@ import com.pulse.data.remote.api.RestApi
 
 class RecipesRemoteDataSource(private val rm: RestApi) {
 
-    suspend fun getRecipes(page: Int? = 1, pageSize: Int? = 40) = rm.getRecipes(page, pageSize)
+    suspend fun getRecipes(page: Int?, pageSize: Int?) = rm.getRecipes(page, pageSize)
 }
